0602018eb7936dc14e0c286faa6ac88acf975a2c
Sébastien Lardière <s.lardiere@loxodata.com>
2017-02-13 11:51:31 +0100

    Add missing epoch_ns in replication metric
        See verbose output of pgwatch2 daemon

--- pgwatch2/sql/datastore_setup/metric_definitions.sql
+++ pgwatch2/sql/datastore_setup/metric_definitions.sql
@@ -249,6 +249,7 @@ values (
 9.1,
 $sql$
 SELECT
+  (extract(epoch from now()) * 1e9)::int8 as epoch_ns,
   application_name as tag_application_name,
   pg_xlog_location_diff(pg_current_xlog_location(), replay_location)::int8 as lag_b,
   coalesce(client_addr::text, client_hostname) as client_info,
