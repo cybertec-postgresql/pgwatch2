026dabe9866dfa12cee12fd70dd3608c1f8127d9
Kaarel Moppel <kaarel.moppel@gmail.com>
2017-10-30 21:34:32 +0200

    New version of the "backends" metric SQL for PG 10
    Resolves issue #37

--- pgwatch2/sql/datastore_setup/metric_definitions.sql
+++ pgwatch2/sql/datastore_setup/metric_definitions.sql
@@ -48,6 +48,33 @@ select
 $sql$
 );
 
+insert into pgwatch2.metric(m_name, m_pg_version_from,m_sql)
+values (
+'backends',
+10,
+$sql$
+with sa_snapshot as (
+  select * from pg_stat_activity
+  where pid != pg_backend_pid()
+  and datname = current_database()
+  and backend_type = 'client backend'
+)
+select
+  (extract(epoch from now()) * 1e9)::int8 as epoch_ns,
+  (select count(*) from sa_snapshot) as total,
+  (select count(*) from sa_snapshot where state = 'active') as active,
+  (select count(*) from sa_snapshot where state = 'idle') as idle,
+  (select count(*) from sa_snapshot where state = 'idle in transaction') as idleintransaction,
+  (select count(*) from sa_snapshot where wait_event_type is not null) as waiting,
+  (select extract(epoch from (now() - backend_start))::int
+    from sa_snapshot order by backend_start limit 1) as longest_session_seconds,
+  (select extract(epoch from (now() - xact_start))::int
+    from sa_snapshot where xact_start is not null order by xact_start limit 1) as longest_tx_seconds,
+  (select extract(epoch from max(now() - query_start))::int
+    from sa_snapshot where state = 'active') as longest_query_seconds;
+$sql$
+);
+
 /* bgwriter */
 
 insert into pgwatch2.metric(m_name, m_pg_version_from,m_sql)
