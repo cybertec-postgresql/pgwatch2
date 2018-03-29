7675a43141fe65f38547279a53759fea0c260eac
Kaarel Moppel <kaarel.moppel@gmail.com>
2018-03-29 14:05:47 +0300

    "Sproc details" dashboard + metric improved
    Sproc selection now includes full schema name + better grouping intervals for smoother graphs

--- pgwatch2/sql/datastore_setup/metric_definitions.sql
+++ pgwatch2/sql/datastore_setup/metric_definitions.sql
@@ -361,6 +361,7 @@ SELECT
   (extract(epoch from now()) * 1e9)::int8 as epoch_ns,
   schemaname::text AS tag_schema,
   funcname::text  AS tag_function_name,
+  quote_ident(schemaname)||'.'||quote_ident(funcname) as tag_function_full_name,
   p.oid as tag_oid, -- for overloaded funcs
   calls as sp_calls,
   self_time,
