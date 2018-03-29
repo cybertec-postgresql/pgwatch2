45c161f178b94881d276f3de352202b056e2fc7c
Kaarel Moppel <kaarel.moppel@gmail.com>
2018-03-29 13:03:50 +0300

    "backends" metric: parallel query workers tracking for PG10
    Distinguish between user's main query and background workers that are launched when "parallel query" takes effect.

--- pgwatch2/sql/datastore_setup/metric_definitions.sql
+++ pgwatch2/sql/datastore_setup/metric_definitions.sql
@@ -57,21 +57,21 @@ with sa_snapshot as (
   select * from get_stat_activity()
   where pid != pg_backend_pid()
   and datname = current_database()
-  and backend_type = 'client backend'
 )
 select
   (extract(epoch from now()) * 1e9)::int8 as epoch_ns,
   (select count(*) from sa_snapshot) as total,
-  (select count(*) from sa_snapshot where state = 'active') as active,
-  (select count(*) from sa_snapshot where state = 'idle') as idle,
-  (select count(*) from sa_snapshot where state = 'idle in transaction') as idleintransaction,
-  (select count(*) from sa_snapshot where wait_event_type is not null) as waiting,
+  (select count(*) from sa_snapshot where backend_type = 'background worker') as background_workers,
+  (select count(*) from sa_snapshot where state = 'active' and backend_type = 'client backend') as active,
+  (select count(*) from sa_snapshot where state = 'idle' and backend_type = 'client backend') as idle,
+  (select count(*) from sa_snapshot where state = 'idle in transaction' and backend_type = 'client backend') as idleintransaction,
+  (select count(*) from sa_snapshot where wait_event_type is not null and backend_type = 'client backend') as waiting,
   (select extract(epoch from (now() - backend_start))::int
-    from sa_snapshot order by backend_start limit 1) as longest_session_seconds,
+    from sa_snapshot where backend_type = 'client backend' order by backend_start limit 1) as longest_session_seconds,
   (select extract(epoch from (now() - xact_start))::int
-    from sa_snapshot where xact_start is not null order by xact_start limit 1) as longest_tx_seconds,
+    from sa_snapshot where xact_start is not null and backend_type = 'client backend' order by xact_start limit 1) as longest_tx_seconds,
   (select extract(epoch from max(now() - query_start))::int
-    from sa_snapshot where state = 'active') as longest_query_seconds;
+    from sa_snapshot where state = 'active' and backend_type = 'client backend') as longest_query_seconds;
 $sql$
 );
 
