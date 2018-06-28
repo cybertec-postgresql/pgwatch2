b01383f2dd90f0602cd4635c41a32f9a6669522e
Kaarel Moppel <kaarel.moppel@gmail.com>
2018-06-28 15:27:29 +0300

    Explicitly using public schema for get_*() helpers

--- pgwatch2/metrics/backends/9.6/metric.sql
+++ pgwatch2/metrics/backends/9.6/metric.sql
@@ -1,5 +1,5 @@
 with sa_snapshot as (
-  select * from get_stat_activity() where pid != pg_backend_pid() and not query like 'autovacuum:%'
+  select * from public.get_stat_activity() where pid != pg_backend_pid() and not query like 'autovacuum:%'
 )
 select
   (extract(epoch from now()) * 1e9)::int8 as epoch_ns,
