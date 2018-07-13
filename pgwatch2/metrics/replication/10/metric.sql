d8b21dd78f0690aa3a7292a45bd96779bb22d317
Kaarel Moppel <kaarel.moppel@gmail.com>
2018-07-13 22:55:19 +0200

    Amend last commit with replication metric for PG10

--- pgwatch2/metrics/replication/10/metric.sql
+++ pgwatch2/metrics/replication/10/metric.sql
@@ -2,7 +2,8 @@ SELECT
   (extract(epoch from now()) * 1e9)::int8 as epoch_ns,
   application_name as tag_application_name,
   coalesce(client_addr::text, client_hostname) as tag_client_info,
-  coalesce(pg_wal_lsn_diff(pg_current_wal_lsn(), replay_lsn)::int8, 0) as lag_b,
+  coalesce(pg_wal_lsn_diff(pg_current_wal_lsn(), write_lsn)::int8, 0) as write_lag_b,
+  coalesce(pg_wal_lsn_diff(pg_current_wal_lsn(), replay_lsn)::int8, 0) as replay_lag_b,
   state
 from
   pg_stat_replication;
