4ee447894177e5e2913ab01101cedbfca47ccd98
Kaarel Moppel <kaarel.moppel@gmail.com>
2018-08-09 15:29:53 +0300

    Metrics: get_table_bloat_approx() made to silently ignore < 9.5 servers. Issue #98

--- pgwatch2/sql/metric_fetching_helpers/table_bloat_approx.sql
+++ pgwatch2/sql/metric_fetching_helpers/table_bloat_approx.sql
@@ -1,5 +1,11 @@
+BEGIN;
+
 CREATE EXTENSION IF NOT EXISTS pgstattuple WITH SCHEMA PUBLIC;
 
+DO $OUTER$
+
+DECLARE
+  l_sproc_text text := $_SQL_$
 CREATE OR REPLACE FUNCTION public.get_table_bloat_approx(OUT approx_free_percent double precision, OUT approx_free_space double precision) AS
 $$
     select
@@ -16,6 +22,19 @@ $$
       and not n.nspname like any (array[E'pg\\_%', 'information_schema'])
       having sum(approx_free_space)::double precision > 0
 $$ LANGUAGE sql SECURITY DEFINER;
+$_SQL_$;
 
-GRANT EXECUTE ON FUNCTION public.get_table_bloat_approx() TO public;
-COMMENT ON FUNCTION public.get_table_bloat_approx() is 'created for pgwatch2';
+BEGIN
+  IF (regexp_matches(
+  		regexp_replace(current_setting('server_version'), '(beta|devel).*', '', 'g'),
+        E'\\d+\\.?\\d+?')
+      )[1]::double precision > 9.4 THEN
+    EXECUTE l_sproc_text;
+
+    EXECUTE 'GRANT EXECUTE ON FUNCTION public.get_table_bloat_approx() TO public;';
+    EXECUTE 'COMMENT ON FUNCTION public.get_table_bloat_approx() is ''created for pgwatch2''';
+  END IF;
+END;
+$OUTER$;
+
+COMMIT;
