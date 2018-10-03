3d9066e07d11c8b64506b4cef6c17ca196cf6772
Kaarel Moppel <kaarel.moppel@gmail.com>
2018-10-03 19:45:08 +0300

    Docker: install psutil_disk_io_total() helper to test DB

--- docker-launcher-nonroot.sh
+++ docker-launcher-nonroot.sh
@@ -68,6 +68,7 @@ fi
 /usr/lib/postgresql/9.5/bin/postgres --single -j -D /var/lib/postgresql/9.5/main -c config_file=/etc/postgresql/9.5/main/postgresql.conf pgwatch2 </pgwatch2/sql/metric_fetching_helpers/psutil_cpu.sql
 /usr/lib/postgresql/9.5/bin/postgres --single -j -D /var/lib/postgresql/9.5/main -c config_file=/etc/postgresql/9.5/main/postgresql.conf pgwatch2 </pgwatch2/sql/metric_fetching_helpers/psutil_mem.sql
 /usr/lib/postgresql/9.5/bin/postgres --single -j -D /var/lib/postgresql/9.5/main -c config_file=/etc/postgresql/9.5/main/postgresql.conf pgwatch2 </pgwatch2/sql/metric_fetching_helpers/psutil_disk.sql
+/usr/lib/postgresql/9.5/bin/postgres --single -j -D /var/lib/postgresql/9.5/main -c config_file=/etc/postgresql/9.5/main/postgresql.conf pgwatch2 </pgwatch2/sql/metric_fetching_helpers/psutil_disk_io_total.sql
 
 if [ -z "$NOTESTDB" ] ; then
 /usr/lib/postgresql/9.5/bin/postgres --single -j -D /var/lib/postgresql/9.5/main -c config_file=/etc/postgresql/9.5/main/postgresql.conf pgwatch2 </pgwatch2/bootstrap/insert_test_monitored_db.sql
