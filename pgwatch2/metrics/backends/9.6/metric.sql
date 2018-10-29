46b28abec94c4a05bb8dcdf36b20d2768aac1d96
Kaarel Moppel <kaarel.moppel@gmail.com>
2018-10-29 17:59:14 +0200

    Metrics: 9.6 "backends.waiting" more exact now

--- pgwatch2/metrics/backends/9.6/metric.sql
+++ pgwatch2/metrics/backends/9.6/metric.sql
@@ -7,7 +7,7 @@ select
   (select count(*) from sa_snapshot where state = 'active') as active,
   (select count(*) from sa_snapshot where state = 'idle') as idle,
   (select count(*) from sa_snapshot where state = 'idle in transaction') as idleintransaction,
-  (select count(*) from sa_snapshot where wait_event_type is not null) as waiting,
+  (select count(*) from sa_snapshot where wait_event_type in ('LWLockNamed', 'Lock', 'BufferPin')) as waiting,
   (select extract(epoch from (now() - backend_start))::int
     from sa_snapshot order by backend_start limit 1) as longest_session_seconds,
   (select extract(epoch from (now() - xact_start))::int
