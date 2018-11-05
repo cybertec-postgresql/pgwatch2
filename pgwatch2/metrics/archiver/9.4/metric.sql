5c0e4b1e7a14e756864fcfdb9a60c6b65340ec48
Kaarel Moppel <kaarel.moppel@gmail.com>
2018-11-05 18:58:32 +0200

    Metrics: "archiver" failing -> is_failing_int

--- pgwatch2/metrics/archiver/9.4/metric.sql
+++ pgwatch2/metrics/archiver/9.4/metric.sql
@@ -2,7 +2,7 @@ select
   (extract(epoch from now()) * 1e9)::int8 as epoch_ns,
   archived_count,
   failed_count,
-  coalesce(last_failed_time, '1970-01-01'::timestamptz) > coalesce(last_archived_time, '1970-01-01'::timestamptz) as failing,
+  case when coalesce(last_failed_time, '1970-01-01'::timestamptz) > coalesce(last_archived_time, '1970-01-01'::timestamptz) then 1 else 0 end as is_failing_int,
   extract(epoch from now() - last_failed_time)::int8 as seconds_since_last_failure
 from
   pg_stat_archiver
