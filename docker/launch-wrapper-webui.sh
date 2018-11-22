b4a29295a0f7fc4f5082937a9559da22a3ee5a5b
Kaarel Moppel <kaarel.moppel@gmail.com>
2018-11-22 11:10:15 +0100

    Fix Web UI Docker image
    As per #119

--- docker/launch-wrapper-webui.sh
+++ docker/launch-wrapper-webui.sh
@@ -5,9 +5,9 @@
 ###
 
 # currently only checks if SSL is enabled and if so generates new cert on the first run
-if [ ! -f /pgwatch2/ssl_key.pem -o ! -f /pgwatch2/ssl_cert.pem ] ; then
-    openssl req -x509 -newkey rsa:4096 -keyout /pgwatch2/self-signed-ssl.key -out /pgwatch2/self-signed-ssl.pem -days 3650 -nodes -sha256 -subj '/CN=pw2'
-    chmod 0600 /pgwatch2/self-signed-ssl.*
+if [ ! -f /pgwatch2/persistent-config/self-signed-ssl.key -o ! -f /pgwatch2/persistent-config/self-signed-ssl.pem ] ; then
+    openssl req -x509 -newkey rsa:4096 -keyout /pgwatch2/persistent-config/self-signed-ssl.key -out /pgwatch2/persistent-config/self-signed-ssl.pem -days 3650 -nodes -sha256 -subj '/CN=pw2'
+    chmod 0600 /pgwatch2/persistent-config/self-signed-ssl.*
 fi
 
 exec /pgwatch2/webpy/web.py "$@"
