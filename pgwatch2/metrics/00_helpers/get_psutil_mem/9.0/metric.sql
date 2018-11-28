ee23ee9149a2a3ef4fe58804ecf2a009e6d921f3
Kaarel Moppel <kaarel.moppel@gmail.com>
2018-11-28 11:50:56 +0200

    Metrics: remove "shared" column from psutil memory metrics
    For some reason "psutil" doesn't provide shared memory info on Redhat systems. Not too critical
    information though so just removing

--- pgwatch2/metrics/00_helpers/get_psutil_mem/9.0/metric.sql
+++ pgwatch2/metrics/00_helpers/get_psutil_mem/9.0/metric.sql
@@ -1,7 +1,7 @@
 /* Pre-requisites: PL/Pythonu and "psutil" Python package (e.g. pip install psutil) */
 
 CREATE OR REPLACE FUNCTION public.get_psutil_mem(
-	OUT total float8, OUT used float8, OUT free float8, OUT shared float8, OUT buff_cache float8, OUT available float8, OUT percent float8,
+	OUT total float8, OUT used float8, OUT free float8, OUT buff_cache float8, OUT available float8, OUT percent float8,
 	OUT swap_total float8, OUT swap_used float8, OUT swap_free float8, OUT swap_percent float8
 )
  LANGUAGE plpythonu
@@ -10,7 +10,7 @@ AS $FUNCTION$
 from psutil import virtual_memory, swap_memory
 vm = virtual_memory()
 sw = swap_memory()
-return vm.total, vm.used, vm.free, vm.shared, vm.buffers + vm.cached, vm.available, vm.percent, sw.total, sw.used, sw.free, sw.percent
+return vm.total, vm.used, vm.free, vm.buffers + vm.cached, vm.available, vm.percent, sw.total, sw.used, sw.free, sw.percent
 $FUNCTION$;
 
 REVOKE EXECUTE ON FUNCTION public.get_psutil_mem() FROM PUBLIC;
--- pgwatch2/sql/datastore_setup/metric_definitions.sql
+++ pgwatch2/sql/datastore_setup/metric_definitions.sql
@@ -1266,7 +1266,7 @@ values (
 $sql$
 SELECT
   (extract(epoch from now()) * 1e9)::int8 as epoch_ns,
-  total, used, free, shared, buff_cache, available, percent,
+  total, used, free, buff_cache, available, percent,
   swap_total, swap_used, swap_free, swap_percent
 from
   public.get_psutil_mem();
