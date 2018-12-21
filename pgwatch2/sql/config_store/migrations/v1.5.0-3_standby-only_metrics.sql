b4f22a9bc8ae7502081cd30cec3d45594221595c
Kaarel Moppel <kaarel.moppel@gmail.com>
2018-12-21 17:46:42 +0200

    Correct v1.5.0 migration script

--- pgwatch2/sql/config_store/migrations/v1.5.0-3_standby-only_metrics.sql
+++ pgwatch2/sql/config_store/migrations/v1.5.0-3_standby-only_metrics.sql
@@ -5,6 +5,6 @@ alter table metric
   add m_standby_only bool default false;
 
 alter table metric
-  add constraint metric_check check (not (m_master_only and m_standby_only))
+  add constraint metric_check check (not (m_master_only and m_standby_only));
 
 commit;
