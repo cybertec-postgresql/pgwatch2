a718e51f76efebec4f985d074714be2d79f19980
Kaarel Moppel <kaarel.moppel@gmail.com>
2019-01-21 11:01:15 +0200

    PG-as-backend: scripts to test storage performance / size

---
+++ pgwatch2/sql/metric_store/storage_test/multiply-pg-test-data.py
@@ -0,0 +1,135 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""This script doubles the existing PG metrics data with each call, using
+intervals found found for each metrics.
+NB! Doubling could meana a lot of data so start conservatively
+"""
+
+import psycopg2
+import psycopg2.extras
+import logging
+import time
+
+## ADJUST AS NEEDED ##
+METRICSCONN_STR = "host=localhost port=5434 dbname=postgres user=pgwatch2"
+HOW_MANY_TIME_TO_DOUBLE = 1   # i.e. when having 1d of data, 5 means there will be 1m (1*2*2*2*2*2)
+LOGLVL = logging.INFO   # logging.DEBUG
+
+SQL_TO_DOUBLE_DATA = """
+with q_interval as (    -- determine interval from 2 sequential rows
+	select * from (
+		select time - lag(time) over (order by time) as metric_interval
+		from public.{metric}
+		where dbname = (select dbname from public.all_distinct_dbname_metrics dd
+                        where exists (select * from public.{metric} where dbname = dd.dbname) limit 1)
+        order by time
+        limit 2
+	) x
+	where metric_interval is not null
+	limit 1
+),
+q_min_max_time as (
+	select max(time) - min(time) as time_diff from public.{metric}
+)
+insert into public.{metric}
+select time+time_diff+metric_interval, dbname, data, tag_data
+from
+	public.{metric},
+	q_min_max_time,
+	q_interval
+order by 1, 2;
+"""
+SQL_SCHEMA_TYPE = "select schema_type from public.storage_schema_type"
+SQL_CREATE_PARTITIONS_METRIC_TIME = """
+with q_timespan as (
+    select
+        max(time)::date+1 as start_time,
+        (max(time) + (max(time) - min(time)) + '1d'::interval)::date as end_time
+    from
+        public.{metric}
+)
+select
+    public.ensure_partition_metric_time('{metric}', gs)
+from 
+    q_timespan, generate_series(start_time, end_time, '1d'::interval) gs
+"""
+SQL_CREATE_PARTITIONS_METRIC_DBNAME_TIME = """
+with q_timespan as (
+    select
+        max(time)::date+1 as start_time,
+        (max(time) + (max(time) - min(time)) + '1d'::interval)::date as end_time
+    from
+        public.{metric}
+),
+q_distinct_dbnames as (
+    select distinct dbname from public.all_distinct_dbname_metrics
+)
+select
+    public.ensure_partition_metric_dbname_time('{metric}', dbname, gs)
+from 
+    q_timespan, q_distinct_dbnames, generate_series(start_time, end_time, '1d'::interval) gs
+"""
+SQL_GET_TOP_LEVEL_metricS = "select table_name from public.get_top_level_metric_tables()"
+SLEEP_SECONDS_BETWEEN_ROUNDS = 5
+
+
+def execute(sql, params=None):
+    result = []
+    
+    conn = psycopg2.connect(METRICSCONN_STR)
+    conn.autocommit = True
+    cur = conn.cursor(cursor_factory=psycopg2.extras.RealDictCursor)
+    cur.execute(sql, params)
+
+    if cur.statusmessage.startswith('SELECT') or cur.description:
+        result = cur.fetchall()
+    else:
+        result = [{'rows_affected': str(cur.rowcount)}]
+    
+    if conn:
+        conn.close()
+    return result
+
+
+if __name__ == '__main__':
+    logging.basicConfig(format='%(asctime)s %(message)s', level=LOGLVL)
+
+    logging.info("testing connection...")
+    execute("select 1")
+    logging.info("connection OK...")
+
+    logging.info("determining schema type...")
+    ret = execute(SQL_SCHEMA_TYPE)
+    schema_type = ret[0]['schema_type']
+    if schema_type not in  ['metric', 'metric-time', 'metric-dbname-time']:
+        raise Exception('Unexcpected schema type:' + schema_type)
+    logging.info("schema type: %s", schema_type)
+
+    logging.info("determining top level tables / metrics...")
+    ret = execute(SQL_GET_TOP_LEVEL_metricS)
+    all_metrics = [x['table_name'].replace('public.', '') for x in ret]
+    logging.info("found %s top level tables: %s", len(all_metrics), all_metrics)
+
+    for i in range(0, HOW_MANY_TIME_TO_DOUBLE):
+        logging.info("starting LOOP %s...", i)
+                
+        for metric in all_metrics:
+            
+            if schema_type == "metric-time":
+                logging.debug("ensuring sub-partitions for next time range...")
+                execute(SQL_CREATE_PARTITIONS_METRIC_TIME.format(metric=metric))
+                logging.debug("done")
+            elif schema_type == "metric-dbname-time":
+                logging.debug("ensuring sub-partitions for time range...")
+                execute(SQL_CREATE_PARTITIONS_METRIC_DBNAME_TIME.format(metric=metric))
+                logging.debug("done")
+            
+            logging.info("duplicating data for %s...", metric)
+            ret = execute(SQL_TO_DOUBLE_DATA.format(metric=metric))
+            logging.info("done. rows added: %s", ret[0]['rows_affected'])
+        
+        logging.info("finished LOOP 1, sleeping %ss...", SLEEP_SECONDS_BETWEEN_ROUNDS)
+        time.sleep(SLEEP_SECONDS_BETWEEN_ROUNDS)
+
+    logging.info("script finished!")
---
+++ pgwatch2/sql/metric_store/storage_test/storage_test.sh
@@ -0,0 +1,27 @@
+###
+### The "test data" mode can be used to quickly generate test data
+### for your chosen data model and planned host count to evaluate I/O
+### capacity and query speed for your hardware.
+### NB! Test mode does not change the metrics counters, just multiplies data,
+### so Grafana dashboards will not be realistic, only "interaction speed"
+###
+
+# Before running pgwatch2 daemon in the test data generation mode, for more accurate results
+# one should install the helpers manually or just use a superuser (then helpers are created automatically
+# in the "ad-hoc mode", which is implicit with "testdata mode"). Also it's recommended to run quite some various
+# queries repeatedly on the target host if it's freshly bootstrapped, otherwise "stat_statements" will not contain
+# any data. Best to use some live server that is planned to be monitored.
+
+# 1. step - initialize a new schema or drop all existing metrics (for example with public.drop_all_metric_tables())
+
+# 2. step - run the pgwatch daemon in the "test data" mode
+# Here we generate 1 week of data for 5 hosts. (could run for a couple of hours!)
+/pgwatch2/pgwatch2 --testdata-days 7 --testdata-multiplier 5 --adhoc-config=exhaustive \
+    --adhoc-conn-str "host=localhost dbname=pgwatch2 user=pgwatch2 sslmode=disable" \   # metrics collection target
+    --pg-metric-store-conn-str="host=localhost port=5434 dbname=postgres sslmode=disable" \ # metrics storage target
+    --pg-schema-type=metric-time --verbose
+
+# 3. as current implementation of generating test data a bit slow, if wanting to simulate longer retention / dozens
+# of hosts, a python script name "multiply-pg-test-data.py" can be used to quickly multiply metrics on the DB level.
+# i.e. generate 1 day of data for 50 hosts and then modify (connect string, multiplication factor) and rund the python
+# scipt. In general 1 week of data for 1 host for "exhaustive" preset config ~ 1GB data.
