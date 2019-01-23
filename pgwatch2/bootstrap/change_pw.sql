e26d0829431c1726e1665f799bc0b06d71fbd229
Kaarel Moppel <kaarel.moppel@gmail.com>
2019-01-23 18:44:06 +0200

    Docker: re-fix bootstrap...

--- pgwatch2/bootstrap/change_pw.sql
+++ pgwatch2/bootstrap/change_pw.sql
@@ -1,2 +1,1 @@
 CREATE ROLE pgwatch2 WITH LOGIN PASSWORD 'pgwatch2admin';  -- NB! change the pw for production
-
--- pgwatch2/bootstrap/create_db_pgwatch.sql
+++ pgwatch2/bootstrap/create_db_pgwatch.sql
@@ -1,3 +1,1 @@
 CREATE DATABASE pgwatch2 OWNER pgwatch2;
-
-alter role pgwatch2 in database pgwatch2 set statement_timeout to '1min';   -- just in case
