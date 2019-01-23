9f30c774ae145abd795b8a14e67d4103aff23358
Kaarel Moppel <kaarel.moppel@gmail.com>
2019-01-23 18:17:04 +0200

    Docker: fix DB setup error

--- pgwatch2/bootstrap/change_pw.sql
+++ pgwatch2/bootstrap/change_pw.sql
@@ -1,3 +1,2 @@
 CREATE ROLE pgwatch2 WITH LOGIN PASSWORD 'pgwatch2admin';  -- NB! change the pw for production
 
-alter role pgwatch2 in database pgwatch2 set statement_timeout to '1min';
--- pgwatch2/bootstrap/create_db_pgwatch.sql
+++ pgwatch2/bootstrap/create_db_pgwatch.sql
@@ -1,1 +1,3 @@
 CREATE DATABASE pgwatch2 OWNER pgwatch2;
+
+alter role pgwatch2 in database pgwatch2 set statement_timeout to '1min';   -- just in case
