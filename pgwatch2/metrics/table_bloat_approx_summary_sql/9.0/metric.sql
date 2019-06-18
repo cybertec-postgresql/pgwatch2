53bdf56f5c5fdff76e5666cfb66ce17d92cec0ef
Kaarel Moppel <kaarel.moppel@gmail.com>
2019-06-18 15:14:44 +0300

    Metrics: "table_bloat_approx_summary_sql" + helper added
    For very large DB-s using "pgstattuple" to determine bloat takes too long

---
+++ pgwatch2/metrics/table_bloat_approx_summary_sql/9.0/metric.sql
@@ -0,0 +1,8 @@
+WITH q_bloat AS (
+    select * from get_table_bloat_approx_sql()
+)
+SELECT
+    (extract(epoch from now()) * 1e9)::int8 as epoch_ns,
+    (select sum(approx_bloat_bytes) from q_bloat) as approx_table_bloat_b,
+    ((select sum(approx_bloat_bytes) from q_bloat) * 100 / pg_database_size(current_database()))::int8 as approx_bloat_percentage
+;
