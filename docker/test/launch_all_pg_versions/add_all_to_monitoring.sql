44becf15b0c686d4c1de5b2ba96039a4bb5674b3
Kaarel Moppel <kaarel.moppel@gmail.com>
2019-10-29 13:48:21 +0200

    Docker test: add helper SQL to add all (master) instances to monitoring

---
+++ docker/test/launch_all_pg_versions/add_all_to_monitoring.sql
@@ -0,0 +1,7 @@
+insert into pgwatch2.monitored_db (md_unique_name, md_preset_config_name, md_config, md_hostname, md_port, md_dbname, md_user, md_password, md_is_superuser)
+select 'pg'||pgver, 'exhaustive', null, 'localhost', '543'||pgver, 'postgres', 'postgres', 'postgres', true
+from unnest(array[90,91,92,93,94,95,96,10,11,12]) as pgver
+where not exists (
+        select * from pgwatch2.monitored_db where (md_unique_name, md_hostname, md_dbname) = ('pg'||pgver, 'localhost', 'postgres')
+    )
+;
