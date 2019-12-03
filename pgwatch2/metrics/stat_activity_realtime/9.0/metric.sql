8d9431e52c908b798d8c82522315e4acf2133853
Kaarel Moppel <kaarel.moppel@gmail.com>
2019-12-03 19:46:36 +0200

    stat_activity_realtime: add equivalent of pg_blocking_pids() for pre 9.6

--- pgwatch2/metrics/stat_activity_realtime/9.0/metric.sql
+++ pgwatch2/metrics/stat_activity_realtime/9.0/metric.sql
@@ -4,12 +4,18 @@ SELECT
     usename::text AS user,
     extract(epoch FROM (now() - query_start))::int AS duration_s,
     waiting::int,
+    case when sa.waiting then
+             (select array_to_string((select array_agg(distinct b.pid order by b.pid) from pg_locks b join pg_locks l on l.database = b.database and l.relation = b.relation
+                                      where l.pid = sa.procpid and b.pid != l.pid and b.granted and not l.granted), ','))
+         else
+             null
+        end as blocking_pids,
     ltrim(regexp_replace(current_query, E'[ \\t\\n\\r]+' , ' ', 'g'))::varchar(200) AS query
 FROM
-    pg_stat_activity
+    pg_stat_activity sa
 WHERE
     current_query <> '<IDLE>'
-    AND procpid <> pg_backend_pid()
+    AND procpid != pg_backend_pid()
     AND datname = current_database()
     AND NOW() - query_start > '500ms'::interval
 ORDER BY
