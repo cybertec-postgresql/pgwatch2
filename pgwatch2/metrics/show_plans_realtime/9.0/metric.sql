b0bd82a63e68003a802b22ceca978f092fb44686
Kaarel Moppel <kaarel.moppel@gmail.com>
2020-01-07 16:00:06 +0200

    Support for the pg_show_plans extension
    "show_plans_realtime" metric + PG dashboard

---
+++ pgwatch2/metrics/show_plans_realtime/9.0/metric.sql
@@ -0,0 +1,26 @@
+/* assumes pg_show_plans extension */
+select
+  max((extract(epoch from now()) * 1e9)::int8) as epoch_ns,
+  max(extract(epoch from now() - query_start))::int as max_s,
+  avg(extract(epoch from now() - query_start))::int as avg_s,
+  count(*),
+  array_to_string(array_agg(distinct usename order by usename), ',') as "users",
+  max(md5(plan)) as tag_hash, /* needed for influx */
+  plan,
+  max(query) as query
+from
+  pg_show_plans p
+  join
+  pg_stat_activity a
+    using (pid)
+where
+  p.pid != pg_backend_pid()
+  and datname = current_database()
+  and now() - query_start > '1s'::interval
+group by
+  plan
+order by
+  max_s desc
+limit
+  10
+;
--- pgwatch2/sql/config_store/metric_definitions.sql
+++ pgwatch2/sql/config_store/metric_definitions.sql
@@ -3790,3 +3790,40 @@ $sql$,
 '{"prometheus_all_gauge_columns": true}',
 true
 );
+
+
+insert into pgwatch2.metric(m_name, m_pg_version_from, m_sql, m_column_attrs, m_master_only)
+values (
+'show_plans_realtime',
+9.0,
+$sql$
+/* assumes pg_show_plans extension */
+select
+  max((extract(epoch from now()) * 1e9)::int8) as epoch_ns,
+  max(extract(epoch from now() - query_start))::int as max_s,
+  avg(extract(epoch from now() - query_start))::int as avg_s,
+  count(*),
+  array_to_string(array_agg(distinct usename order by usename), ',') as "users",
+  max(md5(plan)) as tag_hash, /* needed for influx */
+  plan,
+  max(query) as query
+from
+  pg_show_plans p
+  join
+  pg_stat_activity a
+    using (pid)
+where
+  p.pid != pg_backend_pid()
+  and datname = current_database()
+  and now() - query_start > '1s'::interval
+group by
+  plan
+order by
+  max_s desc
+limit
+  10
+;
+$sql$,
+'{"prometheus_all_gauge_columns": true}',
+true
+);
