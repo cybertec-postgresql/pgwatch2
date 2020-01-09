0560906d2f92b2668390d19b99d9fb68d0bbabf3
Kaarel Moppel <kaarel.moppel@gmail.com>
2020-01-09 16:19:59 +0200

    Metrics: add SMART healt checking metric + Python helper
    Implementation depends on the Smartmontools utility

---
+++ pgwatch2/metrics/smart_health_per_disk/9.1/metric.sql
@@ -0,0 +1,6 @@
+select
+  (extract(epoch from now()) * 1e9)::int8 as epoch_ns,
+  device as tag_device,
+  retcode
+from
+  get_smart_health_per_device();
--- pgwatch2/sql/config_store/metric_definitions.sql
+++ pgwatch2/sql/config_store/metric_definitions.sql
@@ -3825,5 +3825,63 @@ limit
 ;
 $sql$,
 '{"prometheus_all_gauge_columns": true}',
+false
+);
+
+insert into pgwatch2.metric(m_name, m_pg_version_from, m_sql, m_column_attrs, m_is_helper)
+values (
+'get_smart_health_per_device',
+9.1,
+$sql$
+CREATE EXTENSION IF NOT EXISTS plpython3u;
+/*
+  A wrapper around smartmontools to verify disk SMART health for all disk devices. 0 = SMART check PASSED.
+  NB! This helper is always meant to be tested / adjusted to make sure all disk are detected etc.
+  Most likely smartctl privileges must be escalated to give postgres access: sudo chmod u+s /usr/local/sbin/smartctl
+*/
+CREATE OR REPLACE FUNCTION get_smart_health_per_device(OUT device text, OUT retcode int) RETURNS SETOF record AS
+$$
+
+import subprocess
+ret_list = []
+errors=0
+
+#disk_detect_cmd='smartctl --scan | cut -d " " -f3 | grep mega' # for Lenovo ServerRAID M1210
+disk_detect_cmd='lsblk -io KNAME,TYPE | grep '' disk'' | cut -d " " -f1 | sort'
+p = subprocess.run(disk_detect_cmd, stdout=subprocess.PIPE, encoding='utf-8', shell=True)
+if p.returncode != 0:
+    return ret_list
+disks = p.stdout.splitlines()
+
+for disk in disks:
+    # health_cmd = 'smartctl -d $disk -a -q silent /dev/sda' % disk    # for Lenovo ServerRAID M1210 members
+    health_cmd = 'smartctl  -a -q silent /dev/%s' % disk
+    p = subprocess.run(health_cmd, stdout=subprocess.PIPE, encoding='utf-8', shell=True)
+    ret_list.append((disk, p.returncode))
+
+return ret_list
+
+$$ LANGUAGE plpython3u VOLATILE;
+
+GRANT EXECUTE ON FUNCTION get_smart_health_per_device() TO pgwatch2;
+
+COMMENT ON FUNCTION get_smart_health_per_device() is 'created for pgwatch2';
+$sql$,
+'{"prometheus_all_gauge_columns": true}',
 true
+);
+
+insert into pgwatch2.metric(m_name, m_pg_version_from, m_sql, m_column_attrs)
+values (
+'smart_health_per_disk',
+9.1,
+$sql$
+select
+  (extract(epoch from now()) * 1e9)::int8 as epoch_ns,
+  device as tag_device,
+  retcode
+from
+  get_smart_health_per_device();
+$sql$,
+'{"prometheus_all_gauge_columns": true}'
 );
