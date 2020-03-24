9c45a384b52acfa0b6a39baf85793ec55fb72c3d
Kaarel Moppel <kaarel.moppel@gmail.com>
2020-03-24 13:43:28 +0200

    Metrics: show_plans_realtime v10+ shows only 'client backend' queries now
    i.e. no parallel worker stuff

--- pgwatch2/sql/config_store/metric_definitions.sql
+++ pgwatch2/sql/config_store/metric_definitions.sql
@@ -4089,6 +4089,43 @@ $sql$,
 false
 );
 
+insert into pgwatch2.metric(m_name, m_pg_version_from, m_sql, m_column_attrs, m_master_only)
+values (
+'show_plans_realtime',
+10,
+$sql$
+/* assumes pg_show_plans extension */
+select
+  max((extract(epoch from now()) * 1e9)::int8) as epoch_ns,
+  max(extract(epoch from now() - query_start))::int as max_s,
+  avg(extract(epoch from now() - query_start))::int as avg_s,
+  count(*),
+  array_to_string(array_agg(distinct usename order by usename), ',') as "users",
+  max(md5(plan)) as tag_hash, /* needed for influx */
+  plan,
+  max(query) as query
+from
+  pg_show_plans p
+  join
+  pg_stat_activity a
+    using (pid)
+where
+  p.pid != pg_backend_pid()
+  and datname = current_database()
+  and now() - query_start > '1s'::interval
+  and backend_type = 'client backend'
+group by
+  plan
+order by
+  max_s desc
+limit
+  10
+;
+$sql$,
+'{"prometheus_all_gauge_columns": true}',
+false
+);
+
 insert into pgwatch2.metric(m_name, m_pg_version_from, m_sql, m_column_attrs, m_is_helper)
 values (
 'get_smart_health_per_device',
