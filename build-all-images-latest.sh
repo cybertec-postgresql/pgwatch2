924b4a580ed90274283ddc001ca97bae7bb651bb
Kaarel Moppel <kaarel.moppel@gmail.com>
2020-04-29 12:39:20 +0300

    Scripts to build / launch all Docker images - report on errors

--- build-all-images-latest.sh
+++ build-all-images-latest.sh
@@ -2,11 +2,34 @@
 
 echo "building build-docker-daemon (log: build-docker-daemon.log)"
 ./build-docker-daemon.sh &> build-docker-daemon.log
+if [ $? -ne 0 ]; then
+  echo "failed. see log for details"
+else
+  echo "ok"
+fi
+
 echo "building build-docker-influx (log: build-docker-influx.log)"
 ./build-docker-influx.sh &> build-docker-influx.log
+if [ $? -ne 0 ]; then
+  echo "failed. see log for details"
+else
+  echo "ok"
+fi
+
 echo "building build-docker-nonroot (log: build-docker-nonroot.log)"
 ./build-docker-nonroot.sh &> build-docker-nonroot.log
+if [ $? -ne 0 ]; then
+  echo "failed. see log for details"
+else
+  echo "ok"
+fi
+
 echo "building build-docker-postgres (log: build-docker-postgres.log)"
 ./build-docker-postgres.sh &> build-docker-postgres.log
+if [ $? -ne 0 ]; then
+  echo "failed. see log for details"
+else
+  echo "ok"
+fi
 
 echo "done"
