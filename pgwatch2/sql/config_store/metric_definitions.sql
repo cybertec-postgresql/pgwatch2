d0f08401d43a0fdc5a37fe47754e455bf191d0f0
Kaarel Moppel <kaarel.moppel@gmail.com>
2020-06-03 15:44:35 +0300

    Metrics: table_io_stats includes also root partition summarized stats

--- pgwatch2/sql/config_store/metric_definitions.sql
+++ pgwatch2/sql/config_store/metric_definitions.sql
@@ -1173,6 +1173,96 @@ WHERE
 $sql$
 );
 
+insert into pgwatch2.metric(m_name, m_pg_version_from, m_sql)
+values (
+'table_io_stats',
+10,
+$sql$
+with recursive
+    q_root_part as (
+        select c.oid,
+               c.relkind,
+               n.nspname root_schema,
+               c.relname root_relname
+        from pg_class c
+                 join pg_namespace n on n.oid = c.relnamespace
+        where relkind = 'p'
+          and relpersistence != 't'
+          and not exists(select * from pg_inherits where inhrelid = c.oid)
+    ),
+    q_parts (relid, relkind, level, root) as (
+        select oid, relkind, 1, oid
+        from q_root_part
+        union all
+        select inhrelid, c.relkind, level + 1, q.root
+        from pg_inherits i
+                 join q_parts q on inhparent = q.relid
+                 join pg_class c on c.oid = i.inhrelid
+    ),
+    q_tstats as (
+        SELECT (extract(epoch from now()) * 1e9)::int8                as epoch_ns,
+        relid,
+        schemaname::text                                       as tag_schema,
+        relname::text                                          as tag_table_name,
+        quote_ident(schemaname) || '.' || quote_ident(relname) as tag_table_full_name,
+        heap_blks_read,
+        heap_blks_hit,
+        idx_blks_read,
+        idx_blks_hit,
+        toast_blks_read,
+        toast_blks_hit,
+        tidx_blks_read,
+        tidx_blks_hit
+        FROM pg_statio_user_tables
+        WHERE NOT schemaname LIKE E'pg\\_temp%'
+        AND (heap_blks_read > 0 OR heap_blks_hit > 0 OR idx_blks_read > 0 OR idx_blks_hit > 0 OR tidx_blks_read > 0 OR
+           tidx_blks_hit > 0)
+    )
+select
+    epoch_ns,
+    tag_schema,
+    tag_table_name,
+    tag_table_full_name,
+    0 as is_part_root,
+    heap_blks_read,
+    heap_blks_hit,
+    idx_blks_read,
+    idx_blks_hit,
+    toast_blks_read,
+    toast_blks_hit,
+    tidx_blks_read,
+    tidx_blks_hit
+from
+    q_tstats
+
+union all
+
+select * from (
+    select epoch_ns,
+        quote_ident(qr.root_schema) as tag_schema,
+        quote_ident(qr.root_relname) as tag_table_name,
+        quote_ident(qr.root_schema) || '.' || quote_ident(qr.root_relname) as tag_table_full_name,
+        1 as is_part_root,
+        sum(heap_blks_read),
+        sum(heap_blks_hit),
+        sum(idx_blks_read),
+        sum(idx_blks_hit),
+        sum(toast_blks_read),
+        sum(toast_blks_hit),
+        sum(tidx_blks_read),
+        sum(tidx_blks_hit)
+    from
+        q_tstats ts
+        join q_parts qp on qp.relid = ts.relid
+        join q_root_part qr on qr.oid = qp.root
+    where
+        qp.relkind = 'r'
+    group by
+        1, 2, 3, 4
+) x;
+$sql$
+);
+
 /* table_stats */
 
 insert into pgwatch2.metric(m_name, m_pg_version_from, m_sql, m_column_attrs, m_master_only)
