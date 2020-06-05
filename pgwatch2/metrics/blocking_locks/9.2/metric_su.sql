62aa0d5b2de4a0c89aad16a16ec7805dbafc3c71
Kaarel Moppel <kaarel.moppel@gmail.com>
2020-06-05 13:00:15 +0300

    Metrics: fix some column data types for 'blocking_locks'

--- pgwatch2/metrics/blocking_locks/9.2/metric_su.sql
+++ pgwatch2/metrics/blocking_locks/9.2/metric_su.sql
@@ -7,17 +7,17 @@ WITH sa_snapshot AS (
 SELECT
     (extract(epoch from now()) * 1e9)::int8 AS epoch_ns,
     waiting.locktype           AS tag_waiting_locktype,
-    waiting_stm.usename        AS tag_waiting_user,
+    waiting_stm.usename::text  AS tag_waiting_user,
     coalesce(waiting.mode, 'null'::text) AS tag_waiting_mode,
     coalesce(waiting.relation::regclass::text, 'null') AS tag_waiting_table,
     waiting_stm.query          AS waiting_query,
     waiting.pid                AS waiting_pid,
     other.locktype             AS other_locktype,
-    other.relation::regclass   AS other_table,
+    other.relation::regclass::text   AS other_table,
     other_stm.query            AS other_query,
     other.mode                 AS other_mode,
     other.pid                  AS other_pid,
-    other_stm.usename          AS other_user
+    other_stm.usename::text    AS other_user
 FROM
     pg_catalog.pg_locks AS waiting
 JOIN
