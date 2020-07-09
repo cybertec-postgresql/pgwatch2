2236c3a08c3ae770f8c2dca90d0a4e3dbd65b8b1
Kaarel Moppel <kaarel.moppel@gmail.com>
2020-07-09 16:18:08 +0300

    Docker image build scripts: build with --no-cache flag
    Assumedly more secure

--- build-docker-nonroot.sh
+++ build-docker-nonroot.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker build --build-arg GIT_TIME=`git show -s --format=%cI HEAD` --build-arg GIT_HASH=`git show -s --format=%H HEAD` -t cybertec/pgwatch2-nonroot:latest -f docker/Dockerfile-nonroot .
+docker build --no-cache --build-arg GIT_TIME=`git show -s --format=%cI HEAD` --build-arg GIT_HASH=`git show -s --format=%H HEAD` -t cybertec/pgwatch2-nonroot:latest -f docker/Dockerfile-nonroot .
