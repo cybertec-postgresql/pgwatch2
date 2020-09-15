6356e0149831a2c82a0270dce3a43473578e2ae4
Kaarel Moppel <kaarel.moppel@gmail.com>
2020-09-15 17:36:16 +0300

    Config DB: add script to remove pgwatch2 config tables
    In case wanting to completely roll back a pgwatch2 setup. As per #303

---
+++ pgwatch2/sql/config_store/remove_config_schema.sql
@@ -0,0 +1,7 @@
+begin;
+drop table if exists pgwatch2.monitored_db;
+drop table if exists pgwatch2.metric_attribute;
+drop table if exists pgwatch2.metric;
+drop table if exists pgwatch2.preset_config;
+drop table if exists pgwatch2.schema_version;
+commit;
