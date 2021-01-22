5fb2b309dacd151e3df6fcaa0f8df392ba3d3061
Сергей Бурладян <eshkinkot@gmail.com>
2021-01-22 21:14:42 +0300

    Missed typecast

--- pgwatch2/metrics/wal_receiver/10/metric_standby.sql
+++ pgwatch2/metrics/wal_receiver/10/metric_standby.sql
@@ -1,4 +1,4 @@
 select
   (extract(epoch from now()) * 1e9)::int8 as epoch_ns,
-  pg_wal_lsn_diff(pg_last_wal_receive_lsn(), pg_last_wal_replay_lsn()) as replay_lag_b,
+  pg_wal_lsn_diff(pg_last_wal_receive_lsn(), pg_last_wal_replay_lsn())::int8 as replay_lag_b,
   extract(epoch from (now() - pg_last_xact_replay_timestamp()))::int8 as last_replay_s;
