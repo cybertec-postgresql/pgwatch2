e7b88f232fa38f42ab2eb9d1e1d67fa86da060b5
Kaarel Moppel <kaarel.moppel@gmail.com>
2021-02-18 14:18:44 +0200

    Metrics: make "stat_statements" 3x more performant
    Was previously potentially slowish on very large SQL statements as some on disk
    sorting could happen. Also limit the store query size from 16k to 8k
    chars. As per #347

--- pgwatch2/metrics/stat_statements/9.4/metric_su.sql
+++ pgwatch2/metrics/stat_statements/9.4/metric_su.sql
@@ -1,100 +1,144 @@
-with q_data as (
-  select
-    (extract(epoch from now()) * 1e9)::int8 as epoch_ns,
-    queryid::text as tag_queryid,
-    /*
-    NB! if security conscious about exposing query texts replace the below expression with a dash ('-') OR
-    use the stat_statements_no_query_text metric instead, created specifically for this use case.
-    */
-    max(ltrim(regexp_replace(query, E'[ \\t\\n\\r]+' , ' ', 'g')))::varchar(16000) as tag_query,
-    array_to_string(array_agg(distinct quote_ident(pg_get_userbyid(userid))), ',') as users,
-    sum(s.calls)::int8 as calls,
-    round(sum(s.total_time)::numeric, 3)::double precision as total_time,
-    sum(shared_blks_hit)::int8 as shared_blks_hit,
-    sum(shared_blks_read)::int8 as shared_blks_read,
-    sum(shared_blks_written)::int8 as shared_blks_written,
-    sum(shared_blks_dirtied)::int8 as shared_blks_dirtied,
-    sum(temp_blks_read)::int8 as temp_blks_read,
-    sum(temp_blks_written)::int8 as temp_blks_written,
-    round(sum(blk_read_time)::numeric, 3)::double precision as blk_read_time,
-    round(sum(blk_write_time)::numeric, 3)::double precision as blk_write_time
-  from
-    pg_stat_statements s
-  where
-    calls > 5
-    and total_time > 0
-    and dbid = (select oid from pg_database where datname = current_database())
-    and not upper(s.query) like any (array['DEALLOCATE%', 'SET %', 'RESET %', 'BEGIN%', 'BEGIN;',
-      'COMMIT%', 'END%', 'ROLLBACK%', 'SHOW%'])
-  group by
-    queryid
-)
-select * from (
-  select
+WITH q_data AS (
+    SELECT
+        (extract(epoch FROM now()) * 1e9)::int8 AS epoch_ns,
+        queryid::text AS tag_queryid,
+        /*
+         NB! if security conscious about exposing query texts replace the below expression with a dash ('-') OR
+         use the stat_statements_no_query_text metric instead, created specifically for this use case.
+         */
+        array_to_string(array_agg(DISTINCT quote_ident(pg_get_userbyid(userid))), ',') AS users,
+        sum(s.calls)::int8 AS calls,
+        round(sum(s.total_time)::numeric, 3)::double precision AS total_time,
+        sum(shared_blks_hit)::int8 AS shared_blks_hit,
+        sum(shared_blks_read)::int8 AS shared_blks_read,
+        sum(shared_blks_written)::int8 AS shared_blks_written,
+        sum(shared_blks_dirtied)::int8 AS shared_blks_dirtied,
+        sum(temp_blks_read)::int8 AS temp_blks_read,
+        sum(temp_blks_written)::int8 AS temp_blks_written,
+        round(sum(blk_read_time)::numeric, 3)::double precision AS blk_read_time,
+        round(sum(blk_write_time)::numeric, 3)::double precision AS blk_write_time
+    FROM
+        pg_stat_statements s
+    WHERE
+        calls > 5
+        AND total_time > 5
+        AND dbid = (
+            SELECT
+                oid
+            FROM
+                pg_database
+            WHERE
+                datname = current_database())
+            AND NOT upper(s.query::varchar(50))
+            LIKE ANY (ARRAY['DEALLOCATE%',
+                'SET %',
+                'RESET %',
+                'BEGIN%',
+                'BEGIN;',
+                'COMMIT%',
+                'END%',
+                'ROLLBACK%',
+                'SHOW%'])
+        GROUP BY
+            queryid
+),
+q_queryid_text AS (
+    SELECT
+        queryid::text,
+        query::varchar(8000)
+    FROM
+        pg_stat_statements
+    WHERE
+        dbid = (
+            SELECT
+                oid
+            FROM
+                pg_database
+            WHERE
+                datname = current_database()))
+SELECT
+    b.*,
+    (
+        SELECT
+            ltrim(regexp_replace(query, E'[ \\t\\n\\r]+', ' ', 'g'))
+        FROM
+            q_queryid_text
+        WHERE
+            q_queryid_text.queryid = b.tag_queryid
+        LIMIT 1) AS tag_query
+FROM (
+    SELECT
+        *
+    FROM (
+        SELECT
+            *
+        FROM
+            q_data
+        WHERE
+            total_time > 0
+        ORDER BY
+            total_time DESC
+        LIMIT 100) a
+UNION
+SELECT
     *
-  from
-    q_data
-  where
-    total_time > 0
-  order by
-    total_time desc
-  limit 100
-) a
-union
-select * from (
-  select
+FROM (
+    SELECT
+        *
+    FROM
+        q_data
+    ORDER BY
+        calls DESC
+    LIMIT 100) a
+UNION
+SELECT
     *
-  from
-    q_data
-  order by
-    calls desc
-  limit 100
-) a
-union
-select * from (
-  select
+FROM (
+    SELECT
+        *
+    FROM
+        q_data
+    WHERE
+        shared_blks_read > 0
+    ORDER BY
+        shared_blks_read DESC
+    LIMIT 100) a
+UNION
+SELECT
     *
-  from
-    q_data
-  where
-    shared_blks_read > 0
-  order by
-    shared_blks_read desc
-  limit 100
-) a
-union
-select * from (
-  select
+FROM (
+    SELECT
+        *
+    FROM
+        q_data
+    WHERE
+        shared_blks_written > 0
+    ORDER BY
+        shared_blks_written DESC
+    LIMIT 100) a
+UNION
+SELECT
     *
-  from
-    q_data
-  where
-    shared_blks_written > 0
-  order by
-    shared_blks_written desc
-  limit 100
-) a
-union
-select * from (
-  select
+FROM (
+    SELECT
+        *
+    FROM
+        q_data
+    WHERE
+        temp_blks_read > 0
+    ORDER BY
+        temp_blks_read DESC
+    LIMIT 100) a
+UNION
+SELECT
     *
-  from
-    q_data
-  where
-    temp_blks_read > 0
-  order by
-    temp_blks_read desc
-  limit 100
-) a
-union
-select * from (
-  select
-    *
-  from
-    q_data
-  where
-    temp_blks_written > 0
-  order by
-    temp_blks_written desc
-  limit 100
-) a;
+FROM (
+    SELECT
+        *
+    FROM
+        q_data
+    WHERE
+        temp_blks_written > 0
+    ORDER BY
+        temp_blks_written DESC
+    LIMIT 100) a) b;
