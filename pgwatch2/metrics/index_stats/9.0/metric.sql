d6db0a7d7e8f12804cd5d2404b7bd7365fc03a00
Kaarel Moppel <kaarel.moppel@gmail.com>
2021-04-30 16:12:34 +0300

    Metrics: further optimize gathering of index_stats
    Change of "top" criteria a faster query. Returns a bit less data also
    than the previous version.

--- pgwatch2/metrics/index_stats/9.0/metric.sql
+++ pgwatch2/metrics/index_stats/9.0/metric.sql
@@ -1,5 +1,76 @@
+/* NB! does not return all index stats but biggest, top scanned and biggest unused ones */
 WITH q_locked_rels AS (
   select relation from pg_locks where mode = 'AccessExclusiveLock' and granted
+),
+q_index_details AS (
+  select
+    sui.schemaname,
+    sui.indexrelname,
+    sui.relname,
+    sui.indexrelid,
+    coalesce(pg_relation_size(sui.indexrelid), 0) as index_size_b,
+    sui.idx_scan,
+    sui.idx_tup_read,
+    sui.idx_tup_fetch,
+    io.idx_blks_read,
+    io.idx_blks_hit,
+    i.indisvalid,
+    i.indisprimary,
+    i.indisunique
+  from
+    pg_stat_user_indexes sui
+    join pg_statio_user_indexes io on io.indexrelid = sui.indexrelid
+    join pg_index i on i.indexrelid = sui.indexrelid
+  where not sui.schemaname like E'pg\\_temp%'
+  and not exists (select * from q_locked_rels where relation = sui.relid or relation = sui.indexrelid)
+),
+q_top_indexes AS (
+    /* biggest */
+    select *
+    from (
+             select indexrelid
+             from q_index_details
+             where idx_scan > 1
+             order by index_size_b desc
+             limit 200
+         ) x
+    union
+    /* most block traffic */
+    select *
+    from (
+             select indexrelid
+             from q_index_details
+             order by coalesce(idx_blks_read, 0) + coalesce(idx_blks_hit, 0) desc
+             limit 200
+         ) y
+    union
+    /* most scans */
+    select *
+    from (
+             select indexrelid
+             from q_index_details
+             order by idx_scan desc nulls last
+             limit 200
+         ) z
+    union
+    /* biggest unused non-constraint */
+    select *
+    from (
+             select q.indexrelid
+             from q_index_details q
+             where idx_scan = 0
+             and not (indisprimary or indisunique)
+             order by index_size_b desc
+             limit 200
+         ) z
+    union
+    /* all invalid */
+    select *
+    from (
+             select q.indexrelid
+             from q_index_details q
+             where not indisvalid
+         ) zz
 )
 SELECT
   (extract(epoch from now()) * 1e9)::int8 as epoch_ns,
@@ -11,54 +82,16 @@ SELECT
   coalesce(idx_scan, 0) as idx_scan,
   coalesce(idx_tup_read, 0) as idx_tup_read,
   coalesce(idx_tup_fetch, 0) as idx_tup_fetch,
-  coalesce(pg_relation_size(indexrelid), 0) as index_size_b,
-  quote_ident(schemaname)||'.'||quote_ident(sui.indexrelname) as index_full_name_val,
-  md5(regexp_replace(regexp_replace(pg_get_indexdef(sui.indexrelid),indexrelname,'X'), '^CREATE UNIQUE','CREATE')) as tag_index_def_hash,
-  regexp_replace(regexp_replace(pg_get_indexdef(sui.indexrelid),indexrelname,'X'), '^CREATE UNIQUE','CREATE') as index_def,
-  case when not i.indisvalid then 1 else 0 end as is_invalid_int,
-  case when i.indisprimary then 1 else 0 end as is_pk_int,
-  case when i.indisunique then 1 else 0 end as is_uq_or_exc
+  coalesce(index_size_b, 0) as index_size_b,
+  quote_ident(schemaname)||'.'||quote_ident(indexrelname) as index_full_name_val,
+  md5(regexp_replace(regexp_replace(pg_get_indexdef(indexrelid),indexrelname,'X'), '^CREATE UNIQUE','CREATE')) as tag_index_def_hash,
+  regexp_replace(regexp_replace(pg_get_indexdef(indexrelid),indexrelname,'X'), '^CREATE UNIQUE','CREATE') as index_def,
+  case when not indisvalid then 1 else 0 end as is_invalid_int,
+  case when indisprimary then 1 else 0 end as is_pk_int,
+  case when indisunique then 1 else 0 end as is_uq_or_exc
 FROM
-  pg_stat_user_indexes sui
-  JOIN
-  pg_index i USING (indexrelid)
+  q_index_details id
 WHERE
-  relid IN (select *
-            from (select relid
-                  from pg_stat_user_tables
-                  where not schemaname like E'pg\\_temp%'
-                  order by pg_table_size(relid) desc nulls last
-                  limit 200
-                 ) x
-            union
-            select *
-            from (
-                     select relid
-                     from pg_stat_user_tables
-                     where not schemaname like E'pg\\_temp%'
-                     order by coalesce(n_tup_ins, 0) + coalesce(n_tup_upd, 0) + coalesce(n_tup_del, 0) desc
-                     limit 200) y
-            union
-            select *
-            from (
-                     select relid
-                     from pg_stat_user_tables
-                     where not schemaname like E'pg\\_temp%'
-                     and idx_scan > 1
-                     order by idx_scan desc
-                     limit 200) z
-            union
-            select *
-            from (
-                     select relid
-                     from pg_stat_user_indexes
-                     where not schemaname like E'pg\\_temp%'
-                     order by pg_relation_size(indexrelid) desc nulls last
-                     limit 100) w
-  )
-  AND NOT schemaname like E'pg\\_temp%'
-  AND i.indrelid not in (select relation from q_locked_rels)
-  AND i.indexrelid not in (select relation from q_locked_rels)
+  indexrelid IN (select indexrelid from q_top_indexes)
 ORDER BY
-  schemaname, relname, indexrelname;
-  
+  id.schemaname, id.relname, id.indexrelname;
