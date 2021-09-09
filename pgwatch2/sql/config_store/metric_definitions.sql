0c661d4bad61855d101666dca1252f95df50e755
Kaarel Moppel <kaarel.moppel@cognite.com>
2021-09-09 12:20:36 +0300

    v14 metrics: another correction on pg_wal_stats.wal_bytes handling

--- pgwatch2/sql/config_store/metric_definitions.sql
+++ pgwatch2/sql/config_store/metric_definitions.sql
@@ -6842,7 +6842,7 @@ select
     (extract(epoch from now()) * 1e9)::int8 as epoch_ns,
     wal_records,
     wal_fpi,
-    wal_bytes,
+    (wal_bytes / 1024)::int8 as wal_bytes_kb,
     wal_buffers_full,
     wal_write,
     wal_sync,
