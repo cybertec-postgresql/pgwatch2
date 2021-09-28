268b2b6f3a6172535d51def171377c0a0211c2dd
Kaarel Moppel <kaarel.moppel@cognite.com>
2021-09-28 14:21:31 +0200

    Metrics: add new metrics for the upcoming Postgres v14 release.
    Some new views are more nische, so don't add the to default configs though.

---
+++ pgwatch2/metrics/wal_stats/14/metric.sql
@@ -0,0 +1,12 @@
+select
+    (extract(epoch from now()) * 1e9)::int8 as epoch_ns,
+    wal_records,
+    wal_fpi,
+    (wal_bytes / 1024)::int8 as wal_bytes_kb,
+    wal_buffers_full,
+    wal_write,
+    wal_sync,
+    wal_write_time::int8,
+    wal_sync_time::int8
+from
+    pg_stat_wal;
