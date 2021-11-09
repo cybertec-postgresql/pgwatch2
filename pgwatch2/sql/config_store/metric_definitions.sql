58118f31eb7a47ac219a078b6a9968599aa3e886
Kaarel Moppel <kaarel.moppel@cognite.com>
2021-11-09 15:28:03 +0200

    Metrics: add "wait_events"

--- pgwatch2/sql/config_store/metric_definitions.sql
+++ pgwatch2/sql/config_store/metric_definitions.sql
@@ -6854,6 +6854,32 @@ $sql$
 );
 
 
+insert into pgwatch2.metric(m_name, m_pg_version_from, m_sql)
+values (
+'wait_events',
+14,
+$sql$
+  with q_sa as (
+      select * from pg_stat_activity where datname = current_database() and pid <> pg_backend_pid()
+  )
+  select
+    (extract(epoch from now()) * 1e9)::int8 as epoch_ns,
+    wait_event_type as tag_wait_event_type,
+    wait_event as tag_wait_event,
+    count(*),
+    case when wait_event_type is not null then avg(abs(1e6* extract(epoch from now() - query_start)))::int8 else null end as avg_query_duration_us,
+    (select count(*) from q_sa where state = 'active') as total_active
+  from
+    q_sa
+  where
+    state = 'active'
+    and wait_event_type is not null
+  group by
+    1, 2, 3;
+$sql$
+);
+
+
 /* Metric attributes */
 -- truncate pgwatch2.metric_attribute;
 
