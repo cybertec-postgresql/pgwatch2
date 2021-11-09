b69c3b550ea7f7cb8a74d77aa77422febe82255f
Kaarel Moppel <kaarel.moppel@cognite.com>
2021-11-09 13:01:37 +0200

    Metrics: add "catalog_size_b" to "db_size"
    This will help in detecting catalog bloat that can massively slow down
    session startup

--- pgwatch2/metrics/db_size/9.0/metric.sql
+++ pgwatch2/metrics/db_size/9.0/metric.sql
@@ -1,3 +1,4 @@
 select
   (extract(epoch from now()) * 1e9)::int8 as epoch_ns,
-  pg_database_size(current_database()) as size_b;
+  pg_database_size(current_database()) as size_b,
+  (select sum(pg_total_relation_size(oid)) from pg_class where relkind = 'r' and relname LIKE E'pg\\_%') as catalog_size_b;
--- pgwatch2/sql/config_store/metric_definitions.sql
+++ pgwatch2/sql/config_store/metric_definitions.sql
@@ -783,7 +783,8 @@ values (
 $sql$
 select
   (extract(epoch from now()) * 1e9)::int8 as epoch_ns,
-  pg_database_size(current_database()) as size_b;
+  pg_database_size(current_database()) as size_b,
+  (select sum(pg_total_relation_size(oid)) from pg_class where relkind = 'r' and relname LIKE E'pg\\_%') as catalog_size_b;
 $sql$,
 '{"prometheus_all_gauge_columns": true}'
 );
