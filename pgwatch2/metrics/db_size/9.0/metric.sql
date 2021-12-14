b7cc94b8cc25714ab30510c2b950042c0e24ad7a
Kaarel Moppel <kaarel.moppel@cognite.com>
2021-12-14 16:19:24 +0200

    Metrics: update db_size.catalog_size_b calculation

--- pgwatch2/metrics/db_size/9.0/metric.sql
+++ pgwatch2/metrics/db_size/9.0/metric.sql
@@ -1,4 +1,7 @@
 select /* pgwatch2_generated */
   (extract(epoch from now()) * 1e9)::int8 as epoch_ns,
   pg_database_size(current_database()) as size_b,
-  (select sum(pg_total_relation_size(oid))::int8 from pg_class where relkind = 'r' and relname LIKE E'pg\\_%') as catalog_size_b;
+  (select sum(pg_total_relation_size(c.oid))::int8
+   from pg_class c join pg_namespace n on n.oid = c.relnamespace
+   where nspname = 'pg_catalog' and relkind = 'r'
+  ) as catalog_size_b;
--- pgwatch2/sql/config_store/metric_definitions.sql
+++ pgwatch2/sql/config_store/metric_definitions.sql
@@ -784,7 +784,10 @@ $sql$
 select
   (extract(epoch from now()) * 1e9)::int8 as epoch_ns,
   pg_database_size(current_database()) as size_b,
-  (select sum(pg_total_relation_size(oid)) from pg_class where relkind = 'r' and relname LIKE E'pg\\_%') as catalog_size_b;
+  (select sum(pg_total_relation_size(c.oid))::int8
+   from pg_class c join pg_namespace n on n.oid = c.relnamespace
+   where nspname = 'pg_catalog' and relkind = 'r'
+  ) as catalog_size_b;
 $sql$,
 '{"prometheus_all_gauge_columns": true}'
 );
