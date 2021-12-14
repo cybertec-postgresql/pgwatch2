6f0920985b2250cd901f7e9fdf002cb2829dfa87
Kaarel Moppel <kaarel.moppel@cognite.com>
2021-12-14 18:04:32 +0200

    Metrics: make db_size_approx more precise - count also for Catalog TOAST

--- pgwatch2/metrics/db_size_approx/9.1/metric.sql
+++ pgwatch2/metrics/db_size_approx/9.1/metric.sql
@@ -6,8 +6,13 @@ select /* pgwatch2_generated */
     where c.relpersistence != 't'
   ) as size_b,
   current_setting('block_size')::int8 * (
-    select sum(relpages)
+    select sum(c.relpages + coalesce(ct.relpages, 0) + coalesce(cti.relpages, 0))
     from pg_class c
     join pg_namespace n on n.oid = c.relnamespace
+    left join pg_class ct on ct.oid = c.reltoastrelid
+    left join pg_index ti on ti.indrelid = ct.oid
+    left join pg_class cti on cti.oid = ti.indexrelid
     where nspname = 'pg_catalog'
+    and (c.relkind = 'r'
+      or c.relkind = 'i' and not c.relname ~ '^pg_toast')
   ) as catalog_size_b;
