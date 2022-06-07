0c4bf66c4c16019b4cda48cedf2a0d9df6ffe21d
Kaarel Moppel <kaarel.moppel@cognite.com>
2022-06-07 14:35:19 +0300

    Metrics: allow running without errors on upcoming v15
    pg_backup_start_time() function was removed. Tested on REL_15_BETA1.

---
+++ pgwatch2/metrics/db_stats/15/metric_su.sql
@@ -0,0 +1,34 @@
+select /* pgwatch2_generated */
+  (extract(epoch from now()) * 1e9)::int8 as epoch_ns,
+  numbackends,
+  xact_commit,
+  xact_rollback,
+  blks_read,
+  blks_hit,
+  tup_returned,
+  tup_fetched,
+  tup_inserted,
+  tup_updated,
+  tup_deleted,
+  conflicts,
+  temp_files,
+  temp_bytes,
+  deadlocks,
+  blk_read_time,
+  blk_write_time,
+  extract(epoch from (now() - coalesce((pg_stat_file('postmaster.pid', true)).modification, pg_postmaster_start_time())))::int8 as postmaster_uptime_s,
+  checksum_failures,
+  extract(epoch from (now() - checksum_last_failure))::int8 as checksum_last_failure_s,
+  case when pg_is_in_recovery() then 1 else 0 end as in_recovery_int,
+  system_identifier::text as tag_sys_id,
+  session_time::int8,
+  active_time::int8,
+  idle_in_transaction_time::int8,
+  sessions,
+  sessions_abandoned,
+  sessions_fatal,
+  sessions_killed
+from
+  pg_stat_database, pg_control_system()
+where
+  datname = current_database();
