14d621faec6c5a260ed83935497e486a18e12f8c
Kris Avi <kris@krisavi.com>
2023-05-30 23:41:09 +0300

    Multi-host for configDB, adds target_session_attrs

--- webpy/datadb.py
+++ webpy/datadb.py
@@ -17,10 +17,10 @@ def setConnectionStringForMetrics(conn_string):
     connection_string_metrics = conn_string
 
 
-def setConnectionString(host, port, dbname, username, password, require_ssl=False, connect_timeout=10):
+def setConnectionString(host, port, dbname, username, password, target_session_attrs='any', require_ssl=False, connect_timeout=10):
     global connection_string
-    connection_string = 'host={} port={} dbname={} user={} password={} connect_timeout={} {}'.format(
-        host, port, dbname, username, password, connect_timeout, '' if not require_ssl else 'sslmode=require')
+    connection_string = 'host={} port={} dbname={} user={} password={} target_session_attrs={} connect_timeout={} {}'.format(
+        host, port, dbname, username, password, target_session_attrs, connect_timeout, '' if not require_ssl else 'sslmode=require')
 
 
 def getConnection(conn_str=None, autocommit=True):
