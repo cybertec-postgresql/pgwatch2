35fbf791074cc44669eeb5b260bec6d2f23c427e
Marcelo Diaz <marcelorauldiaz@gmail.com>
2024-04-24 10:58:43 -0300

    [-] fix replication slots metrics for replica instances (#768)
    * Fix of replication slots metrics when it's executed on the replica instance and pg_current_wal_lsn execution is not allowed
    * Update pgwatch2/metrics/replication_slots/10/metric_master.sql
    ---------
    Co-authored-by: Pavlo Golub <pavlo.golub@gmail.com>

--- pgwatch2/metrics/replication_slots/10/metric_master.sql
+++ pgwatch2/metrics/replication_slots/10/metric_master.sql
@@ -4,8 +4,10 @@ select /* pgwatch2_generated */
   coalesce(plugin, 'physical')::text as tag_plugin,
   active,
   case when active then 0 else 1 end as non_active_int,
-  pg_wal_lsn_diff(pg_current_wal_lsn(), restart_lsn)::int8 as restart_lsn_lag_b,
-  pg_wal_lsn_diff(pg_current_wal_lsn(), confirmed_flush_lsn)::int8 as confirmed_flush_lsn_lag_b,
+  case when not pg_is_in_recovery() then pg_wal_lsn_diff(pg_current_wal_lsn(), restart_lsn)::int8
+  else pg_wal_lsn_diff(pg_last_wal_replay_lsn(), restart_lsn)::int8 end as restart_lsn_lag_b,
+  case when not pg_is_in_recovery() then pg_wal_lsn_diff(pg_current_wal_lsn(), confirmed_flush_lsn)::int8
+  else pg_wal_lsn_diff(pg_last_wal_replay_lsn(), confirmed_flush_lsn)::int8 end as confirmed_flush_lsn_lag_b,
   greatest(age(xmin), age(catalog_xmin))::int8 as xmin_age_tx
 from
   pg_replication_slots;
