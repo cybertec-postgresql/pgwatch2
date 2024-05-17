3a21bbd8aaa347b10d1b748380b7920a30dc3ebc
Pavlo Golub <pavlo.golub@gmail.com>
2024-05-17 13:51:41 +0200

    [-] fix FREEZE age for partitioned tables in `table_stats`, fixes #760

--- pgwatch2/metrics/table_stats/10/metric.sql
+++ pgwatch2/metrics/table_stats/10/metric.sql
@@ -48,7 +48,7 @@ with recursive /* pgwatch2_generated */
                autovacuum_count,
                analyze_count,
                autoanalyze_count,
-               age(c.relfrozenxid) as tx_freeze_age
+               case when c.relkind != 'p' then age(c.relfrozenxid) else 0 end as tx_freeze_age
         from pg_stat_user_tables ut
             join pg_class c on c.oid = ut.relid
             left join pg_class t on t.oid = c.reltoastrelid
